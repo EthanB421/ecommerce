0fd64d42014551952a5687cff5b128b4918f953b
Ethan Bautista <89360058+EthanB421@users.noreply.github.com>
2024-09-04 15:30:03 -0700

    initialized project

---
+++ ecommerceProj/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ecommerceProj.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
